84bf4c295a222c130bcfdab9f26d088949e3d49d
Adrian Vizcaino Del Rio <avizcaino@utb.edu.co>
2024-10-10 18:00:00 -0500

    Update db.js

--- app/models/db.js
+++ app/models/db.js
@@ -8,4 +8,10 @@ var connection = mysql.createPool({
   database: dbConfig.DB
 });
 
+// open the MySQL connection
+connection.connect(error => {
+  if (error) throw error;
+  console.log("Successfully connected to the database.");
+});
+
 module.exports = connection;
